64786f57d4bf218d56f7b8a6265619285c572ae9
HENRY TEICHEIRA <hteicheira@gmail.com>
2018-10-03 20:09:40 -0700

    Added scroll feature to js file

--- index.js
+++ index.js
@@ -33,3 +33,20 @@ $('#carouselExample').on('slide.bs.carousel', function(e) {
     }
   }
 });
+
+$(function() {
+  $('nav a').bind('click', function(event) {
+    event.preventDefault();
+    var $anchor = $(this);
+    console.log($anchor.attr('href'));
+    $('html, body')
+      .stop()
+      .animate(
+        {
+          scrollTop: $($anchor.attr('href')).offset().top
+        },
+        600
+      );
+    event.preventDefault();
+  });
+});
